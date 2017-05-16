527ce3292ac4c6ba69d42b230be0100cca70f52e
Charpell <charpellumeh@gmail.com>
2017-05-15 20:03:28 -0700

    Write Algorithm for Prime numbers

---
+++ primeNos.js
@@ -0,0 +1,37 @@
+'use strict'
+
+module.exports.primeNos = function(n){
+	var primes = [];
+
+	function isPrime(num){
+		var check = [];
+		for(var j=2;j<num;j++){
+			if(num%j==0){
+				check.push(j);
+			}
+		}
+		if(check.length>0){
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
+
+	if(isNaN(n)){
+		return "Invalid number";
+	}
+	else if(n<2){
+		return [];
+	}
+	else{
+		for(var k=2;k<(n+1);k++){
+			if(isPrime(k)){
+				primes.push(k);
+			}
+		}
+	}
+	return primes;
+
+	
+}
