6e1de2477e259dd6145845ebe9ea9548e1499cc2
Charpell <charpellumeh@gmail.com>
2017-05-16 10:03:45 -0700

    Remove test.js file

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -15,7 +15,7 @@ describe("Prime Numbers Generator", function() {
       });
 
         it("should return [] for -30", function() {
-        expect(primeNos.primeNos(-30).length).toBe(0);
+        expect(primeNos.primeNos(-12).length).toBe(0);
       });
 
       it("should return [] for 0", function() {
@@ -33,6 +33,9 @@ describe("Prime Numbers Generator", function() {
         expect(primeNos.primeNos(3).length).toBe(2);
       });
 
+      it("should return [2,3] for 4", function() {
+        expect(primeNos.primeNos(4).length).toBe(2);
+      });
       it("should return [ ] for Negative number", function() {
         expect(primeNos.primeNos(5).length).toBe(3);
       });
@@ -41,6 +44,9 @@ describe("Prime Numbers Generator", function() {
         expect(primeNos.primeNos(6).length).toBe(3);
       });
 
+      it("should return [2,3,5,7] for 8", function() {
+        expect(primeNos.primeNos(8).length).toBe(4);
+      });
       it("should return 'Invalid number' for 'notANumber'", function() {
         expect(primeNos.primeNos(['notANumber'])).toBe('Invalid number');
       });
